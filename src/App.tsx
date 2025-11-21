adbd7f20fbf4b23a40dadb1a8c49e98ab1e34a73
Deepesh Nimma <deepnimma@gmail.com>
2025-11-21 15:08:36 -0500

    website

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+    return (
+        <body>
+            <div className={"header-container"}>
+                <div className={"title"}>
+                    NottAnotherMasterSetTracker
+                </div>
+                <div className={"titleSubtext"}>
+                    Find all the cards you need!
+                </div>
+            </div>
+        </body>
+    )
+}
+
+export default App
