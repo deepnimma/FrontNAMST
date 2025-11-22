b7304a181edf512d8d292052583b4056fde80823
Deepesh Nimma <deepnimma@gmail.com>
2025-11-22 16:56:10 -0500

    Animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,6 +14,7 @@ const Header: React.FC<HeaderProps> = ({ handleReset }) => {
     const [mainHeaderIndex, setMainHeaderIndex] = useState(0);
     const [currentMainHeader, setCurrentMainHeader] = useState(mainHeaderPhrases[0]);
     const [animationStopped, setAnimationStopped] = useState(false);
+    const [isAnimating, setIsAnimating] = useState(false);
 
     useEffect(() => {
         if (animationStopped) return;
@@ -33,14 +34,46 @@ const Header: React.FC<HeaderProps> = ({ handleReset }) => {
     }, [animationStopped]);
 
     useEffect(() => {
+        setIsAnimating(true);
+        const timer = setTimeout(() => {
+            setIsAnimating(false);
+        }, 500); // Animation duration
         setCurrentMainHeader(mainHeaderPhrases[mainHeaderIndex]);
+        return () => clearTimeout(timer);
     }, [mainHeaderIndex]);
+
+    const renderEmphasizedHeader = (headerText: string) => {
+        const emphasisWords = ["PokeDex", "TrainerDex", "IllustratorDex", "MasterSet"];
+        let renderedText: (string | JSX.Element)[] = [headerText];
+
+        emphasisWords.forEach(word => {
+            renderedText = renderedText.flatMap(part => {
+                if (typeof part === 'string') {
+                    const regex = new RegExp(`(${word})`, 'gi');
+                    const matches = part.split(regex);
+                    return matches.map((match, index) =>
+                        regex.test(match) ? (
+                            <span key={`${word}-${index}`} className="emphasized-header-word">
+                                {match}
+                            </span>
+                        ) : (
+                            match
+                        )
+                    );
+                }
+                return part;
+            });
+        });
+        return renderedText;
+    };
 
     return (
         <div className="header-container">
             <button className="title-button" onClick={handleReset}>
                 <h1 className="title">
-                    <span className="title-desktop">{currentMainHeader}</span>
+                    <span className={`title-desktop ${isAnimating ? 'header-fade-in' : ''}`}>
+                        {renderEmphasizedHeader(currentMainHeader)}
+                    </span>
                     <span className="title-mobile">NottDex</span>
                 </h1>
             </button>
