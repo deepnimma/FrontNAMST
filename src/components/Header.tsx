237296be7205f476199f2ed177de2be467112e73
Deepesh Nimma <deepnimma@gmail.com>
2025-11-22 16:54:52 -0500

    Changing header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface HeaderProps {
     handleReset: () => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ handleReset }) => {
+    const mainHeaderPhrases = [
+        "NottAnotherPokeDexTracker",
+        "NottAnotherTrainerDexTracker",
+        "NottAnotherIllustratorDexTracker",
+        "NottAnotherMasterSetTracker"
+    ];
+    const [mainHeaderIndex, setMainHeaderIndex] = useState(0);
+    const [currentMainHeader, setCurrentMainHeader] = useState(mainHeaderPhrases[0]);
+    const [animationStopped, setAnimationStopped] = useState(false);
+
+    useEffect(() => {
+        if (animationStopped) return;
+
+        const interval = setInterval(() => {
+            setMainHeaderIndex(prevIndex => {
+                const nextIndex = prevIndex + 1;
+                if (nextIndex === mainHeaderPhrases.length - 1) {
+                    setAnimationStopped(true);
+                    clearInterval(interval);
+                }
+                return nextIndex % mainHeaderPhrases.length;
+            });
+        }, 3000); // Change every 3 seconds
+
+        return () => clearInterval(interval);
+    }, [animationStopped]);
+
+    useEffect(() => {
+        setCurrentMainHeader(mainHeaderPhrases[mainHeaderIndex]);
+    }, [mainHeaderIndex]);
+
     return (
         <div className="header-container">
             <button className="title-button" onClick={handleReset}>
                 <h1 className="title">
-                    <span className="title-desktop">NottAnotherMasterSetTracker</span>
+                    <span className="title-desktop">{currentMainHeader}</span>
                     <span className="title-mobile">NottDex</span>
                 </h1>
             </button>
-            <div className="titleSubtext">Find all the cards you need!</div>
         </div>
     );
 };
