878ab45b9305fa92f758541dcdb54a5322794c7b
Deepesh Nimma <deepnimma+macbook@gmail.com>
2025-12-18 11:44:18 -0500

    limit to cards

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,7 +19,7 @@ export const parseTags = (tagsString: string): string[] => {
 // Helper to generate TCGPlayer link
 export const generateTcgplayerLink = (setName: string, cardTitle: string): string => {
     const searchString = `${cardTitle.toLowerCase().replace(/-/g, ' ')} ${setName.toLowerCase().replace(/-/g, ' ')}`;
-    const tcgplayerLink = `https://www.tcgplayer.com/search/pokemon/product?productLineName=pokemon&q=${searchString}&view=grid&productTypeName=cards`;
+    const tcgplayerLink = `https://www.tcgplayer.com/search/pokemon/product?productLineName=pokemon&q=${searchString}&view=grid&productTypeName=Cards`;
     const encodedTcgplayerLink = encodeURIComponent(tcgplayerLink);
 
     return `https://partner.tcgplayer.com/namst?u=${encodedTcgplayerLink}`;
